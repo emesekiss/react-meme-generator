d4627aff3742214163e17262406230d7bf0a2630
Emese Kiss <emesekiss92@yahoo.com>
2020-09-24 16:28:13 +0200

    Fix change meme button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Header from './Header';
+import axios from 'axios';
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 function App() {
+  //image will be the final image to display with or without lines
+  const [image, setImage] = useState(null);
+
+  // the lines can be given by the user input: on change they update to the changed value
+  const [line1, setLine1] = useState('');
+  const [line2, setLine2] = useState('');
+
+  //Gives me the url from the array of objects from .blank
+  const [templates, setTemplates] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.memegen.link/templates').then((res) => {
+      setTemplates(res.data);
+      setImage(res.data[0].blank);
+      // console.log(res.data); will give me an array of objects with all the data I need
+    });
+  }, []);
+  // empty dependency array means this effect will only run once (like componentDidMount in classes)
+
+  const handleChangeMeme = () => {
+    const templateUrl = templates[getRandomInt(0, templates.length)].blank;
+    setImage(templateUrl);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="react-root">
+      <Header />
+      <div className="content">
+        <img
+          src={
+            line1 || line2 ? `${image.slice(0, -4)}/${line1}/${line2}` : image
+          }
+          alt="meme"
+        />
+        <button onClick={handleChangeMeme}>Change Meme</button>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={
+            line1 || line2 ? `${image.slice(0, -4)}/${line1}/${line2}` : image
+          }
+          download
         >
-          Learn React
+          Click to Download Meme
         </a>
-      </header>
+      </div>
+      <div>
+        <div className="lines">
+          <label htmlFor="line1">Line 1 </label>
+          <input
+            id="line1"
+            value={line1}
+            onChange={(e) => setLine1(e.target.value)}
+          />
+          <label htmlFor="line2">Line 2</label>
+          <input
+            id="line2"
+            value={line2}
+            onChange={(e) => setLine2(e.target.value)}
+          />
+        </div>
+      </div>
     </div>
   );
 }
