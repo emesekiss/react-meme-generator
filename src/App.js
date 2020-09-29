e4b94269fb60f516b99e0879c510ce19178cb93e
Emese Kiss <emesekiss92@yahoo.com>
2020-09-29 21:13:07 +0200

    Add corrections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
-import Header from './Header';
 import axios from 'axios';
+
+import Header from './Header';
+import './App.css';
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
